29f1c234a22c546e2089bec39653de0aa3a9b2a5
Thierry Degeling <thierry.degeling@govcert.etat.lu>
2019-07-29 00:24:48 +0200

    test vue instances

---
+++ resources/js/components/Orders.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="TheatreSelectContainer">
+        <div v-for="(name, idTheatre) in orders">
+            <input
+                type="checkbox"
+                :id="idTheatre"
+                :value="idTheatre"
+                v-model="checked"
+                @change="updateTheatres">
+            <label
+                :for="idTheatre">
+                {{ name }}
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheatreSelect',
+        props: [
+            'displayTheatres'
+        ],
+        methods: {
+            updateTheatres: function() {
+                this.$emit('updateTheatres', this.proxy)
+            }
+        },
+        computed: {
+            theatres () {
+                return window.theatres
+            },
+            checked: {
+                get() {
+                    return this.displayTheatres;
+                },
+                set(value) {
+                    this.proxy = value;
+                }
+            }
+        },
+        data() {
+            return {
+                proxy: false
+            }
+        }
+    }
+</script>
