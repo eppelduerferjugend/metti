fc706813c0e583e5b52c0dfc89d8a1dd4941f8c6
Fränz Friederes <fraenz@frieder.es>
2022-08-08 01:28:27 +0200

    Fix cache control config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  api: {
-    headers: [
-      // The following example instructs the Edge Network to cache the response
-      // for 5 seconds.
+  async headers () {
+    return [
       {
-        key: 'Cache-Control',
-        value: 's-maxage=5'
+        source: '/api(.*)',
+        headers: [
+          // The following example instructs the Edge Network to cache the
+          // response for 5 seconds.
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=5'
+          }
+        ]
       }
     ]
   }
